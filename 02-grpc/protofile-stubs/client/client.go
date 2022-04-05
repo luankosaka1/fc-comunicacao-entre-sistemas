6f5f77012482c40a6694b29b59e5f70199bef1e5
Luan Kosaka <luan.kosaka@gmail.com>
2022-04-04 21:20:47 -0300

    create grpc client

---
+++ 02-grpc/protofile-stubs/client/client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/luankosaka1/golang-protofile-stubs/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC Server: %v", err)
+	}
+
+	// close the connection when no one uses it
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Joao",
+		Email: "j@j.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
