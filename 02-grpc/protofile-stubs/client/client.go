c67ee5af045aa5aaae275a1269e240d1b6615de4
Luan Kosaka <luan.kosaka@gmail.com>
2022-04-21 08:41:10 -0300

    create proto buffer bidirecional

--- 02-grpc/protofile-stubs/client/client.go
+++ 02-grpc/protofile-stubs/client/client.go
@@ -25,6 +25,7 @@ func main() {
 	AddUser(client)
 	AddUserVerbose(client)
 	AddUsers(client)
+	AddUserStreamBoth(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -97,4 +98,53 @@ func AddUsers(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserStreamBoth(client pb.UserServiceClient) {
+	stream, err := client.AddUserStreamBoth(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "w1",
+			Name:  "Luan",
+			Email: "luan@mail.com",
+		},
+		&pb.User{
+			Id:    "w2",
+			Name:  "kosaka",
+			Email: "kosaka@mail.com",
+		},
+	}
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending user: ", req.Name)
+			stream.Send(req)
+			time.Sleep(time.Second * 2)
+		}
+		stream.CloseSend()
+	}()
+
+	// utilizamos o channel para não deixar aplicação finalizar, assim evitamos que finalize os streams
+	wait := make(chan int)
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error receiving data: %v", err)
+				break
+			}
+			fmt.Printf("Recebendo user %v com status: %v", res.GetUser().GetName(), res.GetStatus())
+		}
+		close(wait)
+	}()
+
+	<-wait
 }
