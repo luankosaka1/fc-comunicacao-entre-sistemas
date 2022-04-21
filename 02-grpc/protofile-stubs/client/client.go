9ebc57ea7ba4a2d147e29075741ac28ca4acf286
Luan Kosaka <luan.kosaka@gmail.com>
2022-04-21 08:18:47 -0300

    add client stream

--- 02-grpc/protofile-stubs/client/client.go
+++ 02-grpc/protofile-stubs/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/luankosaka1/golang-protofile-stubs/pb"
 	"google.golang.org/grpc"
@@ -23,6 +24,7 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
 	AddUser(client)
 	AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -63,4 +65,36 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status:", stream.Status)
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "w1",
+			Name:  "Luan",
+			Email: "luan@mail.com",
+		},
+		&pb.User{
+			Id:    "w2",
+			Name:  "kosaka",
+			Email: "kosaka@mail.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
